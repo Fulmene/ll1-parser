fdf1fbd37ddb38bcfd5cff3d517582825bbb1b05
Ada Joule <adelaide.8000@gmail.com>
2016-11-27 19:56:10 +0700

    Define context-free grammar

---
+++ Grammar/CFG.hs
@@ -0,0 +1,76 @@
+module CFG (
+emptySymbol, eofSymbol,
+fromString, nonterminal, terminal,
+source, result,
+variables, terminals, rules, starter
+) where
+
+import Data.List
+
+data Symbol = Nonterminal String | Terminal String deriving Show
+emptySymbol = Terminal "empty"
+eofSymbol = Terminal "eof"
+
+fromString :: String -> Symbol
+fromString s@(x:xs) = if x == '<' then Nonterminal s else Terminal s
+
+nonterminal :: Symbol -> Bool
+nonterminal (Nonterminal _) = True
+nonterminal _ = False
+
+terminal :: Symbol -> Bool
+terminal (Terminal _) = True
+terminal _ = False
+
+type Rule = (Symbol, [Symbol])
+source :: Rule -> Symbol
+source = fst
+result :: Rule -> [Symbol]
+result = snd
+
+ruleValid :: [String] -> Bool
+ruleValid rs
+  | length rs < 3 = False
+  | rs !! 1 /= "::=" = False
+  | otherwise = True
+
+data CFG = CFG [Symbol] [Symbol] [Rule] Symbol deriving Show
+nonterminals (CFG nts _ _ _) = nts
+terminals (CFG _ ts _ _) = ts
+rules (CFG _ _ rs _) = rs
+starter (CFG _ _ _ st) = st
+
+readCFG :: String -> CFG
+readCFG rules = CFG (filter nonterminal symbols)
+  (filter terminal symbols)
+  (concat $ map readRule $ lines rules)
+  (head symbols) where
+    symbols = map fromString (
+      (nubOrd $
+      filter (\x -> x /= "::=" && x /= "|")
+      (concat $ filter ruleValid (map words (lines rules))))) ++ [eofSymbol]
+
+readRule :: String -> [Rule]
+readRule r = readRule' (words r) where
+  readRule' rs
+    | terminal (fromString $ head rs) = error "Terminal appears on the left hand side."
+    | ruleValid rs = map (\x -> (fromString (head rs), map fromString x)) (splitOn (=="|") (tail $ tail rs))
+    | otherwise = []
+
+------------ Utility functions ------------
+
+splitOn :: (a -> Bool) -> [a] -> [[a]]
+splitOn p xs = case dropWhile p xs of
+                 [] -> []
+                 xs' -> w : splitOn p xs''
+                   where (w, xs'') = break p xs'
+
+nubOrd :: (Eq a, Ord a) => [a] -> [a]
+nubOrd [] = []
+nubOrd xs = x' : nubOrd' xs' x' where
+  (x':xs') = sort xs
+  nubOrd' [] _ = []
+  nubOrd' (x:xs) h
+    | h == x    = nubOrd' xs h
+    | otherwise = x : nubOrd' xs x
+
