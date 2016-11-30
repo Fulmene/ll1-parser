b7152465acf9a92e7c3b27308a17241f2fb6cf94
Ada Joule <adelaide.8000@gmail.com>
2016-11-30 09:27:23 +0700

    Add main file

---
+++ Main.hs
@@ -0,0 +1,21 @@
+import System.IO
+
+import Grammar.CFG
+import Grammar.LL1Parser
+
+main = do
+  args <- getArgs
+  if length args == 2 then
+    hGrammar <- openFile (args !! 0) ReadMode
+    hText <- openFile (args !! 1) ReadMode
+    grammar <- hGetContents hGrammar
+    text <- hGetContents hText
+    if parseProperly (readCFG grammar) text then
+      putStrLn "Accepted"
+    else
+      putStrLn "Rejected"
+    hClose hGrammar
+    hClose hText
+  else
+    putStrLn "Usage: parser [grammar-file] [text-file]"
+
